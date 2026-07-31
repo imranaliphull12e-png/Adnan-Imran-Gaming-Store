72dbb8c0972edbf1d021f412ff23c6c581615bac
Adnan Imran <imranaliphull12e@gmail.com>
2026-07-31 00:58:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,39 @@
 
 console.log("Hello Imran");
 
-alert("Welcome to 3 Brothers Gaming Buyer UAE");
+window.addEventListener("load", function () {
 
-const cards = document.querySelectorAll(".card");
+  const loader = document.getElementById("loader");
 
-cards.forEach(function(card) {
-    card.addEventListener("click", function() {
-        console.log("Card Clicked");
+  if (loader) {
+    setTimeout(function () {
+      loader.style.opacity = "0";
+
+      setTimeout(function () {
+        loader.style.display = "none";
+      }, 500);
+
+    }, 3000);
+  }
+
+  const buttons = document.querySelectorAll(".read-more");
+
+  buttons.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+      const text = this.parentElement;
+
+      text.classList.toggle("show-text");
+
+      if (text.classList.contains("show-text")) {
+        this.textContent = "Read Less";
+      } else {
+        this.textContent = "Read More";
+      }
+
     });
+
+  });
+
 });
